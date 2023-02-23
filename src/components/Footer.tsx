41412caa4b8ec0d808b56ce1449e80feb5eb892f
WiljamiT <74732018+WiljamiT@users.noreply.github.com>
2023-02-23 12:25:34 +0200

    changes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,46 +1,45 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 export const Footer = () => {
+    const [commits, setCommits] = useState<CommitData[]>([]);
+
+
+
+    useEffect(() => {
+        axios
+          .get<CommitData[]>("https://api.github.com/repos/WiljamiT/t3app/commits")
+          .then((response) => {
+            setCommits(response.data);
+            console.log(response.data)
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
+
     return (
         <footer className="relative bg-primary pt-8 pb-6">
         <div className="container mx-auto px-4">
-            <div className="flex flex-wrap text-left lg:text-left">
-            <div className="w-full lg:w-6/12 px-4">
-                <h4 className="text-3xl font-semibold text-primary-content">T3 App</h4>
-                <h5 className="text-lg mt-0 mb-2 text-primary-content">
-                Lorem ipsum.
-                </h5>
-                <div className="mt-6 lg:mb-0 mb-6">
-                <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i></i></button><button className="bg-white text-primary-content shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i></i></button><button className="bg-white text-primary-content shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    <i></i></button><button className="bg-white text-primary-content shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2" type="button">
-                    
-                </button>
-                </div>
+        <div>
+            
+            <ul>
+                {commits.slice(0, 1).map((commit) => (
+                <li key={commit.sha}>
+                    <p>
+                        Site updated and built with commit: <a className="commit" href={commit.html_url}>{commit.sha.slice(0, 7)}</a> at: {new Intl.DateTimeFormat("en-GB", {
+                            timeZone: "Europe/London",
+                            dateStyle: "medium",
+                            timeStyle: "medium",
+                        }).format(new Date(commit.commit.committer.date))}
+                        <p>by: <a href={commit.author.html_url} className="name">{commit.commit.committer.name}</a> </p>
+                        </p>
+                </li>
+                ))}
+            </ul>
             </div>
-            <div className="w-full lg:w-6/12 px-4">
-                <div className="flex flex-wrap items-top mb-6">
-                <div className="w-full lg:w-4/12 px-4 ml-auto"> 
-                </div>
-                <div className="w-full lg:w-4/12 px-4">
-                    <span className="block uppercase text-primary-content text-sm font-semibold mb-2">Header</span>
-                    <ul className="list-unstyled">
-                    <li>
-                        <p className="text-primary-content hover:text-blueGray-800 font-semibold block pb-2 text-sm">text</p>
-                    </li>
-                    <li>
-                        <p className="text-primary-content hover:text-blueGray-800 font-semibold block pb-2 text-sm">Text</p>
-                    </li>
-                    <li>
-                        <p className="text-primary-content hover:text-blueGray-800 font-semibold block pb-2 text-sm">Text</p>
-                    </li>
-                    <li>
-                        <p className="text-primary-content hover:text-blueGray-800 font-semibold block pb-2 text-sm">Text</p>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-            </div>
+            
+            
             <hr className="my-6 border-blueGray-300" />
             <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
